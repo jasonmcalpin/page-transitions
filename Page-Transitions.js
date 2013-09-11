2d94552b1deb69a108ab9538476016892ac077b9
Jason <jason@chaiboy.com>
2013-09-11 16:14:01 -0400

    optimizations for detecting body

--- Page-Transitions.js
+++ Page-Transitions.js
@@ -9,151 +9,150 @@
 */
 
 
-//config
 
-var transitionTargetDefault = 'body';
-var transitionTargetOpacity = 0;
-var bodyElement = document.getElementsByTagName(transitionTargetDefault);
-var elem = bodyElement[0];
-
-function init() {
+(function(){
 	'use strict';
-	// quit if this function has already been called
-	if (initDone) {return;}
-
-	// flag this function so we don't do the same thing twice
-	var initDone = true;
-
-	// kill the timer
-	if (_timer) {clearInterval(_timer);}
-
-	// make body hidden
-
-
-	hideBody();
-	transitionIn();
-	detectButtons();
-}
-
-/* for Mozilla/Opera9 */
-if (document.addEventListener) {
-	document.addEventListener("DOMContentLoaded", init, false);
-}
-
-/* for Internet Explorer */
-/*@cc_on @*/
-/*@if (@_win32)
-document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
-var script = document.getElementById("__ie_onload");
-script.onreadystatechange = function() {
-	if (this.readyState == "complete") {
-		init(); // call the onload handler
+	if(window.console){
+		var	goose = console;
+	} else {
+		var	goose ={'log':''};
 	}
-};
-/*@end @*/
+	var transitionTargetDefault = 'body',
+		transitionTargetOpacity = 0,
+		initDone = false,
+		bodyElement, elem;
 
-/* for Safari */
-if (/WebKit/i.test(navigator.userAgent)) { // sniff
-	var _timer = setInterval(function() {
-		'use strict';
-		if (/loaded|complete/.test(document.readyState)) {
+
+	function init() {
+
+		// quit if this function has already been called
+		if (initDone === true) {
+			goose.log('already run');
+			return;
+		}
+
+		// flag this function so we don't do the same thing twice
+		initDone = true;
+
+		// kill the timer
+		if (_timer) {clearInterval(_timer);}
+
+		
+		//setup transition target
+		bodyElement = document.getElementsByTagName(transitionTargetDefault);
+		elem = bodyElement[0];
+
+		// make body hidden
+		hideBody();
+		transitionIn();
+		detectButtons();
+	}
+
+	/* for Mozilla/Opera9 */
+	if (document.addEventListener) {
+		document.addEventListener("DOMContentLoaded", init, false);
+	}
+
+	/* for Internet Explorer */
+	/*@cc_on @*/
+	/*@if (@_win32)
+	document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
+	var script = document.getElementById("__ie_onload");
+	script.onreadystatechange = function() {
+		if (this.readyState == "complete") {
 			init(); // call the onload handler
 		}
-	}, 10);
-}
+	};
+	/*@end @*/
 
-/* for other browsers */
-window.onload = init;
-
-function callback(e) {
-	'use strict';
-	var button, linkLocation;
-	// deal with events called events or e
-	if (!e) {var e = window.event;}
-
-	// dealing with target and srcElement
-	if (e.target) {button = e.target;}
-	else if (e.srcElement) {button = e.srcElement;}
+	/* for Safari */
+	if (/WebKit/i.test(navigator.userAgent)) { // sniff
+		var _timer = setInterval(function() {
 	
-	// check if a button, if not toss it out
-	if (button.tagName !== 'A'){
-		return;
+			if (/loaded|complete/.test(document.readyState)) {
+				init(); // call the onload handler
+			}
+		}, 10);
 	}
 
-	button.preventDefault();
-	linkLocation = button.href;
-	transitionOut(linkLocation);
-}
+	/* for other browsers */
+	window.onload = init;
 
+	function callback(e) {
 
-function hideBody(){
-	'use strict';
-	elem.style.opacity = 0.0;
-}
+		var button, linkLocation;
+		// deal with events called events or e
+		if (!e) {var e = window.event;}
 
-function detectButtons(){
-	'use strict';
-	// add callback to click event of all links.
-	if (document.addEventListener){
-		document.addEventListener('click', callback, false);}
-	else
-		{document.attachEvent('onclick', callback);}
-}
+		// dealing with target and srcElement
+		if (e.target) {button = e.target;}
+		else if (e.srcElement) {button = e.srcElement;}
+		
+		// check if a button, if not toss it out
+		if (button.tagName !== 'A'){
+			return;
+		}
 
-
-function transitionIn(){
-	'use strict';
-	var _timer = setInterval( countUp(), 1000);
-}
-
-function transitionOut(linkPassed){
-	'use strict';
-	var _timer = setInterval( countDown(), 500);
-	redirectPage(linkPassed);
-}
-
-function countUp(){
-	'use strict';
-
-	if(transitionTargetOpacity<1){
-		transitionTargetOpacity+=0.025;
-		elem.style.opacity = transitionTargetOpacity;
-	} else{
-		clearInterval(_timer);
+		button.preventDefault();
+		linkLocation = button.href;
+		transitionOut(linkLocation);
 	}
-}
 
-function countDown(){
-	'use strict';
-	if(transitionTargetOpacity>0){
-		transitionTargetOpacity-=0.025;
-		elem.style.opacity = transitionTargetOpacity;
-	} else{
-		clearInterval(_timer);
+
+	function hideBody(){
+
+		elem.style.opacity = 0.0;
+		goose.log('hide body');
 	}
-}
 
-function redirectPage(linkPassed) {
-	'use strict';
-	window.location = linkPassed;
-}   
+	function detectButtons(){
 
-/* fade in page depreciated
-
-jQuery( function() {
-	// fade in page for browsers that don't support pageshow
-	if (jQuery.browser.opera || jQuery.browser.msie){
-			transitionIn();
+		// add callback to click event of all links.
+		if (document.addEventListener){
+			document.addEventListener('click', callback, false);}
+		else
+			{document.attachEvent('onclick', callback);}
 	}
-	// fade page out on click 
-	jQuery("a").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		transitionOut();
-	});
 
-}); //ready end
-jQuery(window).bind('pageshow', function() {
-		transitionIn();
-});
-     */
+
+	function transitionIn(){
+
+		var _timer = setInterval( countUp, 1);
+		goose.log('fade in');
+	}
+
+	function transitionOut(linkPassed){
+
+		var _timer = setInterval( countDown, 1);
+		redirectPage(linkPassed);
+		goose.log('fade out');
+	}
+
+	function countUp(){
+
+
+		if(transitionTargetOpacity<1){
+			transitionTargetOpacity+=0.025;
+			elem.style.opacity = transitionTargetOpacity;
+		} else{
+			clearInterval(_timer);
+		}
+	}
+
+	function countDown(){
+
+		goose.log('counting down');
+		if(transitionTargetOpacity>0){
+			transitionTargetOpacity-=0.025;
+			elem.style.opacity = transitionTargetOpacity;
+		} else{
+			clearInterval(_timer);
+		}
+	}
+
+	function redirectPage(linkPassed) {
+
+		window.location = linkPassed;
+	}   
+
+})();
